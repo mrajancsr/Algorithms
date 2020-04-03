f7b10c8ee14f545307481b2f7f0517ba4065b2f0
RajanS <mrajancsr@gmail.com>
2020-04-02 20:45:02 -0400

    implemented dictionaries with a regular list

--- map_dictionary/map_base.py
+++ map_dictionary/map_base.py
@@ -21,4 +21,49 @@ class MapBase(MutableMapping):
 
 class UnsortedTableMap(MapBase):
 	"""map implementation using an unordered list"""
-	
+	def __init__(self):
+		self._table = []
+
+	def __getitem__(self, k):
+		"""return value associated with key k (raise keyError if not found)
+			if found, takes O(1) time.  Otherwise:
+        	O(n) time
+        """
+		for item in self._table:
+			if item._key == k:
+				return item._value 
+		raise KeyError("key Error: " + repr(k))
+
+	def __setitem__(self, k, v):
+		for item in self._table:
+			if item._key == k:
+				item._value = v 
+				return 
+		self._table.append(self.Item(k, v))
+
+	def __delitem__(self, k, v):
+		for idx, item in enumerate(self._table):
+			if item._key == k:
+				self._table.pop(idx)
+				return 
+		raise KeyError("key Error: " + repr(k))
+
+	def __len__(self):
+		return len(self._table)
+
+	def __iter__(self):
+		for item in self._table:
+			yield item._key
+	def keys(self):
+		return tuple(i for i in self)
+
+class HashMapBase(MapBase):
+	"""Abstract base class for map using hash-table with MAD compression"""
+	pass
+def TestUnsortedTableMap():
+	chk = UnsortedTableMap()
+	chk['hello'] = 'there'
+	chk[3] = 5
+	print(chk.keys())
+
+TestUnsortedTableMap()
